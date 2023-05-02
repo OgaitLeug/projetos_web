87f5a845c98201dbdff5e6f2c253a4fd490d5db7
TiagoMiguelR <122323570+ogaitleugim@users.noreply.github.com>
2023-05-02 22:41:46 +0100

    Initial commit

---
+++ modulo4/aula_028/script.js
@@ -0,0 +1,10 @@
+/*
+document.getElementsByClassName()
+document.getElementsByName()
+document.getElementsByTagName()
+*/
+
+let e = document.getElementsByClassName("outra-noticia")
+//console.log(e)
+
+e[0].innetText = "Nova noticia"
