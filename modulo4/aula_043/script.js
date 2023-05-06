e0881a2ac29261dc4062061244c42a0db23bc645
TiagoMiguelR <122323570+ogaitleugim@users.noreply.github.com>
2023-05-06 17:38:48 +0100

    update
    exercicios java

--- modulo4/aula_043/script.js
+++ modulo4/aula_043/script.js
@@ -1,1 +1,55 @@
 // INTRODUÇÃO À VALIDAÇÃO DE FORMULÁRIOS
+
+//usuario
+document.querySelector('input[name="usuario"]').addEventListener('keyup', () =>{
+    if(document.querySelector('input[name="usuario"] + span' ) !== null){
+        document.querySelector('input[name="usuario"] + span' ).remove()
+    }
+})
+
+//senha
+document.querySelector('input[name="senha"]').addEventListener('keyup', () =>{
+    if(document.querySelector('input[name="senha"] + span' ) !== null){
+        document.querySelector('input[name="senha"] + span' ).remove()
+    }
+})
+
+
+document.formulario_login.addEventListener('submit', (event) => {
+
+    //buscar valores dos campos
+    let usuario = event.target.usuario.value
+    let senha = event.target.senha.value
+    let submit = true
+
+    // remover erros anteriores
+
+    document.querySelectorAll('.error').forEach(e => e.remove())
+
+    // validar usuario
+    if (usuario === ""){
+        submit = false
+
+        // erro usuario
+        let tmp = document.querySelector('input[name="usuario"]')
+        tmp.insertAdjacentHTML('afterend', '<span class="error">Usuário é obrigatório!</span>')
+    }
+
+    // validar senha
+    if (senha === ""){
+        submit = false
+
+        //erro senha
+        let tmp = document.querySelector('input[name="senha"]')
+        tmp.insertAdjacentHTML('afterend', '<span class="error">O campo senha é obrigatório!</span>')
+    }
+
+    // verificar se o formulário pode ser submetido
+
+    if(!submit){
+        event.preventDefault()
+
+       
+    }
+
+})
--- modulo4/aula_045/script.js
+++ modulo4/aula_045/script.js
@@ -1,4 +1,19 @@
-// este código vai ser executado antes dos elementos do body existire,
-document.querySelector("button").addEventListener('click', () => {
-    document.querySelector('h1').innerText = "Texto do título alterado"
+// // este código vai ser executado antes dos elementos do body existire,
+
+// document.addEventListener('readystatechange', (event) =>{
+//     if(event.target.readyState === 'complete'){ // complete ou interactive funcionam os 2 mas sao diferentes
+
+
+//         document.querySelector("button").addEventListener('click', () => {
+//     document.querySelector('h1').innerText = "Texto do título alterado"
+// })
+
+
+//     }
+// })
+
+document.addEventListener('DOMContentLoaded', () =>{
+    document.querySelector("button").addEventListener('click', () => {
+        document.querySelector('h1').innerText = "Texto do título alterado"
+    })
 })
