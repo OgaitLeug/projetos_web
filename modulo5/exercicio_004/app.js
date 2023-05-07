41dd8d3d572624c0cdba28d8e631dabc68691cfd
TiagoMiguelR <122363005+OgaitLeug@users.noreply.github.com>
2023-05-07 22:52:18 +0100

    Update app.js

--- modulo5/exercicio_004/app.js
+++ modulo5/exercicio_004/app.js
@@ -10,4 +10,26 @@ Enunciado: TRÊS SLIDERS COM VALORES INDEPENDENTES
 
 ---------------------------------------------------------------------------- */
 
+const range_1 = document.querySelector("#range_1")
+range_1.setAttribute('min',0);
+range_1.setAttribute('max',100);
+range_1.value = 0;
+range_1.addEventListener('input', (event) =>{
+    document.querySelector("#value_1").textContent = event.target.value
+})
 
+const range_2 = document.querySelector("#range_2")
+range_2.setAttribute('min',0);
+range_2.setAttribute('max',100);
+range_2.value = 0;
+range_2.addEventListener('input', (event) =>{
+    document.querySelector("#value_2").textContent = event.target.value
+})
+
+const range_3 = document.querySelector("#range_3")
+range_3.setAttribute('min',0);
+range_3.setAttribute('max',100);
+range_3.value = 0;
+range_3.addEventListener('input', (event) =>{
+    document.querySelector("#value_3").textContent = event.target.value
+})
