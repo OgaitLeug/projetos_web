343b623f07043b05f36e4749baafe359179043cc
TiagoMiguelR <122363005+OgaitLeug@users.noreply.github.com>
2023-05-10 19:21:04 +0100

    exercicios

--- modulo5/exercicio_006/app.js
+++ modulo5/exercicio_006/app.js
@@ -9,3 +9,35 @@ Enunciado:
 
     NOTA: quando uma área é apresentada, as outras devem ficar escondidas
 ---------------------------------------------------------------------------- */
+
+
+//Padrão: Display #INFO 1
+hide_all()
+document.getElementById("info1").style.display = "block";
+
+//  DISPLAY INFO 1
+document.getElementById("tab1").addEventListener( "click", () =>{
+    hide_all();
+    document.getElementById("info1").style.display = "block";
+
+})
+
+// DISPLAY INFO 2
+document.getElementById("tab2").addEventListener( "click", () =>{
+    hide_all();
+    document.getElementById("info2").style.display = "block";
+
+})
+
+// DISPLAY INFO 3
+document.getElementById("tab3").addEventListener( "click", () =>{
+    hide_all();
+    document.getElementById("info3").style.display = "block";
+
+})
+
+function hide_all(){
+    document.querySelectorAll('[id^="info"]').forEach((el) => {
+         el.style.display = "none"
+    })
+}
--- modulo5/exercicio_008/app.js
+++ modulo5/exercicio_008/app.js
@@ -6,3 +6,36 @@ Enunciado:
     A ideia é criar o código javascript que permita que o clique em cada
     um dos botões altere o texto acima para a cor de fundo do botão.
 ---------------------------------------------------------------------------- */
+
+const cores = ['primary', 'info', 'success', 'danger', 'warning'];
+document.querySelectorAll('[id^="btn"]').forEach((btn) => {
+    btn.addEventListener('click', (btn) => {
+        let index = btn.target.id.substring(3, 4) - 1;
+        document.querySelector("h3").className = "text-" + cores[index];
+    })
+})
+
+
+
+
+// --------/---------- 1st try --------/---------- 
+
+// document.getElementById("btn1").addEventListener("click", () =>{
+//     document.querySelector("h3").style.color = "#2487B6"
+// })
+
+// document.getElementById("btn2").addEventListener("click", () =>{
+//     document.querySelector("h3").style.color = "#9933CC"
+// })
+
+// document.getElementById("btn3").addEventListener("click", () =>{
+//     document.querySelector("h3").style.color = "#659800"
+// })
+
+// document.getElementById("btn4").addEventListener("click", () =>{
+//     document.querySelector("h3").style.color = "#CC0000"
+// })
+
+// document.getElementById("btn5").addEventListener("click", () =>{
+//     document.querySelector("h3").style.color = "#D97400"
+// })
